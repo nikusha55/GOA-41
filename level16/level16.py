c45722ea62c3eab639cfc666886f4b777c7f3efc
nikusha55 <nikaedisherashvili55@gmail.com>
2024-06-23 18:11:04 +0400

    second commit

---
+++ level16/level16.py
@@ -0,0 +1,31 @@
+familly = ["nika", "luka", "emiri", "qeti",  ]
+
+print(familly[0])
+print(familly[1])
+print(familly[2])
+print(familly[3])
+
+
+
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, ]
+print(numbers[0])
+print(len(numbers) )
+
+
+nums = [10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20] 
+nums[2] = 1
+nums[4] = 1
+nums[6] = 1
+nums[8] = 1
+nums[10] = 1
+print(nums) 
+
+name = input("Enter your name : ")
+lastname = input("Enter your lastname: ")
+age = int(input("Enter your age : "))
+adress = input("Enter your adress: ")
+email = input("Enter your email: ")
+
+user_input = [name, lastname, age, adress, email]
+
+print(user_input)
