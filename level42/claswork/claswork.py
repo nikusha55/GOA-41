36e2660ec13b650fe9e9016d49501bc66fd2b06a
nikusha55 <nikaedisherashvili55@gmail.com>
2024-08-13 23:26:15 +0400

    ninethy commit

---
+++ level42/claswork/claswork.py
@@ -0,0 +1,8 @@
+inputs = input("Enter Your Name: ")
+
+capitalized_name = inputs.upper()
+print(capitalized_name)
+inputs = input("Enter Your Name: ")
+
+capitalized_name = inputs.capitalize()
+print(capitalized_name)
