d020d7760fe97c5ce62a9b9d6a0ab7c864c5cbb3
nikusha55 <nikaedisherashvili55@gmail.com>
2024-08-29 00:19:29 +0400

    twelfthy commit

--- level48/homework/claswork/codewars.py
+++ level48/homework/claswork/codewars.py
@@ -1,2 +1,11 @@
-def solution(string, ending):
-    return string.endswith(ending)
+def grader(score):
+    if score > 1 or score < 0.6:
+        return "F"
+    elif score >= 0.9:
+        return "A"
+    elif score >= 0.8:
+        return "B"
+    elif score >= 0.7:
+        return "C"
+    else:
+        return "D"
