ad477dc87b748db1c20665cde95775944f9df1a6
nikusha55 <166060954+nikusha55@users.noreply.github.com>
2025-01-19 20:01:57 +0300

    Delete day 85 directory

--- day 85/homework/script2.js
+++
@@ -1,7 +0,0 @@
-let hour = parseInt(readLine(), 10);
-
-if (hour >= 0 && hour <= 11) {
-    console.log("am");
-} else {
-    console.log("pm");
-}
--- day 85/homework/script4.js
+++
@@ -1,3 +0,0 @@
-let number = parseInt(readLine(), 10);
-let result = (number % 2 === 0) ? "ლუწი" : "კენტი";
-console.log(result);
