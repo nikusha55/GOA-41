621e1dff7c9dcafb2e5947a365f91ea88d0b434c
nikusha55 <166060954+nikusha55@users.noreply.github.com>
2025-01-19 20:02:17 +0300

    Delete day 87 directory

--- day 87/claswork/script2.js
+++
@@ -1,7 +0,0 @@
-for (let i = 0; i <= 20; i++) {
-    if (i % 2 === 0) {
-        console.log(i, "luwia");
-    } else {
-        console.log(i, "კენტია");
-    }
-}
--- day 87/homework/script3.js
+++
@@ -1,5 +0,0 @@
-let text = "გამარჯობა";
-
-for (let i = 0; i < text.length; i++) {
-  console.log(text[i]);
-}
